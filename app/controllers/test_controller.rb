688dc397ae57bc5ab41bb0bcf8348f6414997931
Kejmer <kajetanhus@gmail.com>
2020-01-13 23:53:04 +0100

    product

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,5 @@
+class TestController < ApplicationController
+  def index
+    @tekst_na_strone = "to jest przykładowe wykorzystanie zmiennych"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'test#index', as: 'home'
+
+  # match "/test", :to => "test#index", :via => :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :product
 end
