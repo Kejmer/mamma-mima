0bd3915243386d8029607da2931982f6750ffb3d
Kejmer <kajetanhus@gmail.com>
2020-01-21 20:42:30 +0100

    creating association

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,15 @@
 class Department < ApplicationRecord
-  has_many :availabilities
+  has_many :availabilities, dependent: :destroy
+  has_many :products, through: :availabilities
 
   validates :city, :street, presence: true
+
+  after_create :create_empty_products
+
+  def create_empty_products
+    products = Product.all
+    products.each do |p|
+      Availability.create!(ammount: 0, product: p, department: self)
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :availabilities
+
+  validates :name, presence: true
 end
