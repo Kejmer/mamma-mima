982d986b909d106e4a10c6b5aa38006242ecf6a9
Kejmer <kajetanhus@gmail.com>
2020-01-22 11:37:57 +0100

    edit

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,5 +1,7 @@
 class DepartmentsController < ApplicationController
 
+  before_action :set_dept, only: [:show, :edit, :update, :destroy]
+
   def index
     @departments = Department.order(city: :asc)
   end
@@ -13,11 +15,27 @@ class DepartmentsController < ApplicationController
 
   def create
     @department = Department.new(permitted_params(:department))
-    # byebug
     if @department.save
       redirect_to departments_url
     else
       render action: 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @department.update_attributes(permitted_params(:department))
+      redirect_to departments_url
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def set_dept
+    @department = Department.find(params[:id])
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
+
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
-    @products = Product.all
+    @products = Product.order(name: :asc)
   end
 
   def show
@@ -13,9 +16,26 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(permitted_params(:product))
     if @product.save
-      redirect_to product_url
+      redirect_to products_url
     else
       render action: 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(permitted_params(:product))
+      redirect_to products_url
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
